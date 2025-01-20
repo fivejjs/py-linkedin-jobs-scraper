5d247a7731fdc12de72fda0f628ae952afdd2233
jinjun <jsunster@gmail.com>
2025-01-21 00:26:37 +1100

    use UV to manage the dev python env

--- examples/example1.py
+++ examples/example1.py
@@ -1,53 +1,86 @@
+from dotenv import load_dotenv
+# from loguru import logger
 from linkedin_jobs_scraper import LinkedinScraper
-from linkedin_jobs_scraper.events import Events, EventData
+from linkedin_jobs_scraper.events import Events, EventData, EventMetrics
 from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
-from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters, TypeFilters, ExperienceLevelFilters, IndustryFilters
+from linkedin_jobs_scraper.filters import (
+    RelevanceFilters,
+    TimeFilters,
+    TypeFilters,
+    ExperienceLevelFilters,
+    OnSiteOrRemoteFilters,
+    SalaryBaseFilters,
+)
+
+_ = load_dotenv(override=True)
 
 
+# Fired once for each successfully processed job
 def on_data(data: EventData):
-    print('[ON_DATA]', data.title, data.company, data.date, data.link, len(data.description))
+    print(
+        "[ON_DATA]",
+        data.title,
+        data.company,
+        data.company_link,
+        data.date,
+        data.date_text,
+        data.link,
+        data.insights,
+        len(data.description),
+    )
+
+
+# Fired once for each page (25 jobs)
+def on_metrics(metrics: EventMetrics):
+    print("[ON_METRICS]", str(metrics))
 
 
 def on_error(error):
-    print('[ON_ERROR]', error)
+    print("[ON_ERROR]", error)
 
 
 def on_end():
-    print('[ON_END]')
+    print("[ON_END]")
 
 
+# Add event listeners
 scraper = LinkedinScraper(
-    chrome_options=None,  # You can pass your custom Chrome options here
-    max_workers=1,  # How many threads will be spawn to run queries concurrently (one Chrome driver for each thread)
-    slow_mo=1,  # Slow down the scraper to avoid 'Too many requests (429)' errors
+    chrome_executable_path=None,  # Custom Chrome executable path (e.g. /foo/bar/bin/chromedriver)
+    chrome_binary_location=None,  # Custom path to Chrome/Chromium binary (e.g. /foo/bar/chrome-mac/Chromium.app/Contents/MacOS/Chromium)
+    chrome_options=None,  # Custom Chrome options here
+    headless=True,  # Overrides headless mode only if chrome_options is None
+    max_workers=1,  # How many threads will be spawned to run queries concurrently (one Chrome driver for each thread)
+    slow_mo=0.5,  # Slow down the scraper to avoid 'Too many requests 429' errors (in seconds)
+    page_load_timeout=40,  # Page load timeout (in seconds)
 )
+
 
 # Add event listeners
 scraper.on(Events.DATA, on_data)
 scraper.on(Events.ERROR, on_error)
 scraper.on(Events.END, on_end)
 
+
 queries = [
+    Query(options=QueryOptions(limit=27)),  # Limit the number of jobs to scrape
     Query(
+        query="mlops",
         options=QueryOptions(
-            optimize=True,  # Blocks requests for resources like images and stylesheet
-            limit=27  # Limit the number of jobs to scrape
-        )
-    ),
-    Query(
-        query='Engineer',
-        options=QueryOptions(
-            locations=['United States'],
-            optimize=False,
+            locations=["NSW Australia"],
+            apply_link=True,
+            skip_promoted_jobs=True,  # Skip promoted jobs. Default to False.
+            page_offset=2,  # How many pages to skip
             limit=5,
             filters=QueryFilters(
-                company_jobs_url='https://www.linkedin.com/jobs/search/?f_C=1441%2C17876832%2C791962%2C2374003%2C18950635%2C16140%2C10440912&geoId=92000000',  # Filter by companies
+                company_jobs_url="https://www.linkedin.com/jobs/search/?f_C=1441%2C17876832%2C791962%2C2374003%2C18950635%2C16140%2C10440912&geoId=92000000",  # Filter by companies
                 relevance=RelevanceFilters.RECENT,
                 time=TimeFilters.MONTH,
                 type=[TypeFilters.FULL_TIME, TypeFilters.INTERNSHIP],
-                experience=None,
-            )
-        )
+                on_site_or_remote=[OnSiteOrRemoteFilters.REMOTE],
+                experience=[ExperienceLevelFilters.MID_SENIOR],
+                base_salary=SalaryBaseFilters.SALARY_180K,
+            ),
+        ),
     ),
 ]
 
